263582218f0272b3ba4e14203bdd04aca35d583d
EPY <erick.pariona@hotmail.com>
2024-04-13 16:04:57 -0500

    CAMBIOS_13042024

--- appCliente_ConsumerSW/Form1.cs
+++ appCliente_ConsumerSW/Form1.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Formatting;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -22,6 +23,25 @@ namespace appCliente_ConsumerSW
         private void btnGrabar_Click(object sender, EventArgs e)
         {
 
+            string url = "https://localhost:44343/api/alumno/registrar";
+            HttpClient cliente = new HttpClient();
+            Alumno objAlumno = new Alumno();
+            objAlumno.Codigo = 0; //
+            objAlumno.Nombre = "Cesar";
+            objAlumno.ApellidoPaterno = "Gamar";
+            objAlumno.ApellidoMaterno = "Geu";
+            objAlumno.Genero = "M";
+            objAlumno.Documento = "78978978";
+            objAlumno.Estado = 1;//
+            var solicitud = cliente.PostAsync(
+                url, objAlumno, new JsonMediaTypeFormatter()).Result;
+                
+            if (solicitud.IsSuccessStatusCode)
+            {
+                var result = solicitud.Content.ReadAsStringAsync().Result;
+                var respuesta = JsonConvert.DeserializeObject<RespuestaServicio>(result);
+                MessageBox.Show(respuesta.codigo + "-"+ respuesta.mensaje);
+            }
         }
 
         private void btnListar_Click(object sender, EventArgs e)
@@ -29,6 +49,7 @@ namespace appCliente_ConsumerSW
             string url = "https://localhost:44343/api/alumno/lista";
             HttpClient cliente = new HttpClient();
             var request = cliente.GetAsync(url).Result;
+
             if (request.IsSuccessStatusCode)
             {
                 var resultado = request.Content.ReadAsStringAsync().Result;
---
+++ appCliente_ConsumerSW/RespuestaServicio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appCliente_ConsumerSW
+{
+    public  class RespuestaServicio
+    {
+        public int codigo {  get; set; }
+        public string mensaje {  get; set; }
+    }
+}
